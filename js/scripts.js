05db9228cc7c8a0ae46f48566ba9ed0da4b12c02
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2024-02-23 19:56:31 -0300

    Add dark mode
    Add Google Fonts
    
    Refactor highlighting text

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ const links = document.querySelectorAll(".go-to a");
 for (const link of links) {
     link.addEventListener("click", clickHandler);
 }
+
 function clickHandler(e) {
     e.preventDefault();
     const href = this.getAttribute("href");
@@ -12,10 +13,29 @@ function clickHandler(e) {
     });
     highlightText(href);
 }
+
 function highlightText(href) {
-    document.querySelector(href).style.color = '#CCEBF1'
+    const text = document.querySelector(href)
+    text.classList.toggle('lighted-text')
     setTimeout(resetTextColor, 1000, href);
 }
+
 function resetTextColor(href) {
-    document.querySelector(href).style.color = '#000000'
+    const text = document.querySelector(href)
+    text.classList.toggle('lighted-text')
 }
+
+const switchButton = document.getElementById("switch-btn")
+
+switchButton.addEventListener('click', (e) => {
+    document.documentElement.classList.toggle('dark-mode');
+    switchButton.classList.toggle('active')
+    const cards = document.querySelectorAll(".card")
+    cards.forEach(card => {
+        card.classList.toggle('bg-dark')
+        card.classList.toggle('text-white')
+    })
+    const navbar = document.querySelector('nav')
+    navbar.classList.toggle('navbar-dark')
+    navbar.classList.toggle('bg-dark')
+})
