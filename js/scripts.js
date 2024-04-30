04340865bdd2147e6db32def79d07bcc57a3fa02
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2024-04-30 03:29:58 -0300

    Update mailing direction

--- js/scripts.js
+++ js/scripts.js
@@ -62,7 +62,7 @@ contactForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const body = Object.fromEntries(new FormData(e.target))
 
-    postData('http://localhost:80/api/sendmail', body)
+    postData('https://jp-portfolio-backend.vercel.app/api/sendmail', body)
     .then(data => {
         console.log(data);
     });
